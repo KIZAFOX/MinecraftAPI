284d620f395c2dd7471ad04899f834422dc7e7b5
Victor <victor.pernak@hotmail.fr>
2024-10-11 23:30:09 +0200

    :cyclone: file updated

--- src/main/java/fr/kiza/minecraftapi/handler/player/PlayerAction.java
+++ src/main/java/fr/kiza/minecraftapi/handler/player/PlayerAction.java
@@ -1,9 +1,8 @@
 package fr.kiza.minecraftapi.handler.player;
 
-import fr.kiza.minecraftapi.handler.packets.handler.PacketHandler;
 import org.bukkit.entity.Player;
 
 @FunctionalInterface
 public interface PlayerAction {
-    void execute(final Player player, final PacketHandler packetHandler);
+    void execute(final Player player);
 }
