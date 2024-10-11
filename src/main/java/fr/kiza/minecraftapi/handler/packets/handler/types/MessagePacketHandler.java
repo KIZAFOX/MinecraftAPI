b9fd94361d83e1db430a7362d2fb305200fb4e20
Victor <victor.pernak@hotmail.fr>
2024-10-11 23:31:21 +0200

    :tada: MessagePacketHandler added

---
+++ src/main/java/fr/kiza/minecraftapi/handler/packets/handler/types/MessagePacketHandler.java
@@ -0,0 +1,24 @@
+package fr.kiza.minecraftapi.handler.packets.handler.types;
+
+import fr.kiza.minecraftapi.handler.packets.handler.PacketBuilder;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
+
+public class MessagePacketHandler implements PacketBuilder<ClientboundSystemChatPacket> {
+
+    public static final String PACKET_ID = "chat";
+
+    private String message;
+
+    @Override
+    public PacketBuilder<ClientboundSystemChatPacket> message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    @Override
+    public ClientboundSystemChatPacket build() {
+        IChatBaseComponent component = IChatBaseComponent.a(this.message);
+        return new ClientboundSystemChatPacket(component, false);
+    }
+}
