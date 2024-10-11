d8b95b37c9a194bf4d2d7655087df826f75fef84
Victor <victor.pernak@hotmail.fr>
2024-10-11 23:35:54 +0200

    :cyclone: file updated

--- src/main/java/fr/kiza/minecraftapi/handler/packets/PacketFactory.java
+++ src/main/java/fr/kiza/minecraftapi/handler/packets/PacketFactory.java
@@ -3,11 +3,13 @@ package fr.kiza.minecraftapi.handler.packets;
 import fr.kiza.minecraftapi.handler.packets.handler.PacketBuilder;
 import fr.kiza.minecraftapi.handler.packets.handler.types.MessagePacketHandler;
 
+import java.util.Objects;
+
 public class PacketFactory {
-    public static PacketBuilder<?> getBuilder(String packetId){
-        if(packetId.equals(MessagePacketHandler.PACKET_ID)){
-            return new MessagePacketHandler();
+    public static PacketBuilder<?> getBuilder(PacketType packetType){
+        if(Objects.requireNonNull(packetType) == PacketType.MESSAGE_PLAYER) {
+            new MessagePacketHandler();
         }
-        throw new IllegalArgumentException("Unknown packet type: " + packetId);
+        throw new IllegalArgumentException("Unknown packet type: " + packetType);
     }
 }
