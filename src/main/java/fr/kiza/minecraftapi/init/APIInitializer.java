9079466e80109f20769bb2d8094dd7eb7c94fb7e
Victor <victor.pernak@hotmail.fr>
2024-10-11 23:29:01 +0200

    :cyclone: manage the auto registering of listeners/events

--- src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
+++ src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
@@ -1,8 +1,11 @@
 package fr.kiza.minecraftapi.init;
 
 import fr.kiza.minecraftapi.core.Core;
+import fr.kiza.minecraftapi.handler.player.PlayerListener;
+import org.bukkit.event.Event;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class APIInitializer {
@@ -10,8 +13,17 @@ public class APIInitializer {
         final Logger logger = plugin.getLogger();
 
         if(plugin.getClass().isAnnotationPresent(MinecraftAPI.class)){
-            logger.info("API detected on " + plugin.getClass().getSimpleName());
             Core.init(plugin);
+
+            Arrays.stream(plugin.getClass().getDeclaredMethods())
+                    .filter(methods -> methods.isAnnotationPresent(PlayerListener.class))
+                    .forEach(methods -> {
+                        final Class<? extends Event> eventClass = methods.getAnnotation(PlayerListener.class).value();
+                        Core.getInstance().getEventDispatcher().registerEvent(eventClass, plugin);
+                        plugin.getServer().getPluginManager().registerEvents(Core.getInstance().getEventDispatcher(), plugin);
+                    });
+
+            logger.info("API detected on " + plugin.getClass().getSimpleName());
         }else{
             logger.warning("Please put @MinecraftAPI in your main class!");
         }
