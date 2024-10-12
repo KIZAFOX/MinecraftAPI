528af7eb96a68cbfdd613f0638c3ad709bbd4617
Victor <victor.pernak@hotmail.fr>
2024-10-12 09:56:42 +0200

    :cyclone: some method added for title

--- src/main/java/fr/kiza/minecraftapi/handler/packets/handler/PacketBuilder.java
+++ src/main/java/fr/kiza/minecraftapi/handler/packets/handler/PacketBuilder.java
@@ -3,6 +3,30 @@ package fr.kiza.minecraftapi.handler.packets.handler;
 import net.minecraft.network.protocol.Packet;
 
 public interface PacketBuilder<T> {
-    PacketBuilder<T> message(String message);
+
     Packet<?> build();
+
+    default PacketBuilder<T> message(String message){
+        return this;
+    }
+
+    default PacketBuilder<T> title(String title){
+        return this;
+    }
+
+    default PacketBuilder<T> subTitle(String subTitle){
+        return this;
+    }
+
+    default PacketBuilder<T> fadeIn(int fadeIn){
+        return this;
+    }
+
+    default PacketBuilder<T> stay(int stay){
+        return this;
+    }
+
+    default PacketBuilder<T> fadeOut(int fadeOut){
+        return this;
+    }
 }
