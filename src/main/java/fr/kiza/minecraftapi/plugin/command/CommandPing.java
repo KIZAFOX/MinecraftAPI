b3f5dc70fb25666a54479e31de5b1cc61d40f571
KIZA <victor.pernak@hotmail.fr>
2024-10-28 16:25:39 +0100

    :cyclone: file updated

--- src/main/java/fr/kiza/minecraftapi/plugin/command/CommandPing.java
+++ src/main/java/fr/kiza/minecraftapi/plugin/command/CommandPing.java
@@ -3,11 +3,15 @@ package fr.kiza.minecraftapi.plugin.command;
 import fr.kiza.minecraftapi.handler.commands.AbstractCommand;
 import fr.kiza.minecraftapi.handler.commands.handler.CommandRegisterer;
 import fr.kiza.minecraftapi.handler.commands.handler.CommandHandler;
-import fr.kiza.minecraftapi.handler.packets.PacketFactory;
-import fr.kiza.minecraftapi.handler.packets.PacketType;
-import fr.kiza.minecraftapi.handler.packets.sender.PacketSender;
+import fr.kiza.minecraftapi.handler.packet.PacketFactory;
+import fr.kiza.minecraftapi.handler.packet.PacketType;
+import fr.kiza.minecraftapi.handler.packet.sender.PacketSender;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
 
 @CommandRegisterer
 @CommandHandler(
@@ -19,11 +23,20 @@ import org.bukkit.command.CommandSender;
 public class CommandPing extends AbstractCommand {
     @Override
     public boolean execute(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof final Player player)){
+            sender.sendMessage(ChatColor.RED + "This command can only be executed by a player");
+            return true;
+        }
+
         PacketSender.sendPacket(PacketFactory.getBuilder(PacketType.MESSAGE_PLAYER)
-                .message("Pong!")
+                .message("Pong! " + player.getName())
                 .build()
         );
-
         return false;
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return List.of();
     }
 }
