8da2c9530cf2eaf732fb757bbac701b08d595bb2
KIZA <victor.pernak@hotmail.fr>
2024-10-30 00:36:38 +0100

    :zap: file updated

--- src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
+++ src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
@@ -3,13 +3,16 @@ package fr.kiza.minecraftapi.init;
 import fr.kiza.minecraftapi.core.Core;
 import fr.kiza.minecraftapi.module.player.PlayerListener;
 import fr.kiza.minecraftapi.module.tools.logger.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 
 public class APIInitializer {
     public static void init(final JavaPlugin plugin) {
-        if(plugin.getClass().isAnnotationPresent(MinecraftAPI.class)){
+        if(!plugin.getClass().isAnnotationPresent(MinecraftAPI.class)){
+            Logger.print("Please read the doc on the GitHub Repository.", Logger.LoggerLevel.ERROR);
+        }else{
             Logger.print("API detected on " + plugin.getClass().getSimpleName(), Logger.LoggerLevel.INFO);
 
             Core.init(plugin);
@@ -22,8 +25,6 @@ public class APIInitializer {
                     });
 
             Logger.print("API successfully initialized on " + plugin.getClass().getSimpleName(), Logger.LoggerLevel.INFO);
-        }else{
-            Logger.print("Please read the doc on the GitHub Repository.", Logger.LoggerLevel.ERROR);
         }
     }
 }
