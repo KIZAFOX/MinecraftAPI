e59eeb8968fbab159bc80f8f3094caf1101e2860
KIZA <victor.pernak@hotmail.fr>
2024-10-29 21:24:55 +0100

    :cyclone: file updated

--- src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
+++ src/main/java/fr/kiza/minecraftapi/init/APIInitializer.java
@@ -1,30 +1,29 @@
 package fr.kiza.minecraftapi.init;
 
 import fr.kiza.minecraftapi.core.Core;
-import fr.kiza.minecraftapi.handler.player.PlayerListener;
-import org.bukkit.event.Event;
+import fr.kiza.minecraftapi.module.player.PlayerListener;
+import fr.kiza.minecraftapi.module.tools.logger.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 public class APIInitializer {
     public static void init(final JavaPlugin plugin) {
-        final Logger logger = plugin.getLogger();
-
         if(plugin.getClass().isAnnotationPresent(MinecraftAPI.class)){
+            Logger.print("API detected on " + plugin.getClass().getSimpleName(), Logger.LoggerLevel.INFO);
+
             Core.init(plugin);
 
             Arrays.stream(plugin.getClass().getDeclaredMethods())
                     .filter(methods -> methods.isAnnotationPresent(PlayerListener.class))
-                    .forEach(methods -> {
+                    .forEach(_ -> {
                         Core.getInstance().getEventDispatcher().registerEvent(plugin);
                         plugin.getServer().getPluginManager().registerEvents(Core.getInstance().getEventDispatcher(), plugin);
                     });
 
-            logger.info("API detected on " + plugin.getClass().getSimpleName());
+            Logger.print("API successfully initialized on " + plugin.getClass().getSimpleName(), Logger.LoggerLevel.INFO);
         }else{
-            logger.warning("Please put @MinecraftAPI in your main class!");
+            Logger.print("Please read the doc on the GitHub Repository.", Logger.LoggerLevel.ERROR);
         }
     }
 }
