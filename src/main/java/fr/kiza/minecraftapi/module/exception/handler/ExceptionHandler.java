06b2f83e3e4afe3174633f7c8fe9e2d69c98d3e4
KIZA <victor.pernak@hotmail.fr>
2024-10-31 20:30:34 +0100

    :fire: file deleted

--- src/main/java/fr/kiza/minecraftapi/module/exception/handler/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package fr.kiza.minecraftapi.module.exception.handler;
-
-public class ExceptionHandler extends Exception {
-    public ExceptionHandler(final String message) {
-        super(message);
-    }
-}
